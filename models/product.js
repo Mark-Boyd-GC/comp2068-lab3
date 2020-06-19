141aab9bd573850d13afda224d50853343e08237
Mark Boyd <mark.boyd1414@gmail.com>
2020-06-18 20:16:00 -0400

    Lab 4 commit

---
+++ models/product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// You need to create a new schema and assign it the following
+// constant
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
